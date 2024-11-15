8bab43bca6ff8e32bc43a311bd818cff1fe28d58
ArtificialVoid <160069526+ArtificialVoid1@users.noreply.github.com>
2024-11-15 10:23:12 -0500

    Update Console.asm

--- Examples/Console.asm
+++ Examples/Console.asm
@@ -1,12 +1,25 @@
+section .data
+  lookUpDig: db "0123456789"
+
 section .text
   global printf
   type printf,@function
+
+IntToStrLoop:
+  mov ebx, rdx
+  div ebx, 10
+  mov ebp, [lookUpDig + edx]
+  
 
 printf:
   pop rdx ; get argument
   pop rdi ; get arg length
 
+  
+
   mov edx, rdi
   mov ecx, rdx
   mov eax, 4
-  mov edi
+  mov ebx, 1
+  int 0x80
+  ret
